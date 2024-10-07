b72fc7545069e53797b54fb9cd1b1c70865cb0b8
shamcalcetas <calcetasshamelle@gmail.com>
2024-10-07 23:20:34 +0800

    docs: adding university_dict.py

---
+++ university_dict.py
@@ -0,0 +1,54 @@
+university_dictOne = {
+                        "ID" : "1",
+                        "Name" : "University of the Philippines",
+                        "Location" : "Quezon City",
+                        "Established Year" : "1908",
+                        "Type" : "Public",
+                        "Website" : "www.up.edu.ph"
+                    }
+
+print(university_dictOne)
+
+university_dictTwo = {
+                        "ID" : "2",
+                        "Name" : "Ateneo de Manila University",
+                        "Location" : "Quezon City",
+                        "Established Year" : "1859",
+                        "Type" : "Private",
+                        "Website" : "www.ateneo.edu"
+                    }
+
+print(university_dictTwo)
+
+university_dictThree = {
+                          "ID" : "3",
+                          "Name" : "De La Salle University",
+                          "Location" : "Manila",
+                          "Established Year" : "1911",
+                          "Type" : "Private",
+                          "Website" : "www.dlsu.edu.ph"
+                      }
+
+print(university_dictThree)
+
+university_dictFour = {
+                        "ID" : "4",
+                        "Name" : "University of Santo Tomas",
+                        "Location" : "Manila",
+                        "Established Year" : "1611",
+                        "Type" : "Private",
+                        "Website" : "www.ust.edu.ph"
+                    }
+
+print(university_dictFour)
+
+university_dictFive = {
+                        "ID" : "5",
+                        "Name" : "Polytechnic University of the Philippines ",
+                        "Location" : "Manila",
+                        "Established Year" : "1904 ",
+                        "Type" : "Public",
+                        "Website" : "www.pup.edu.ph"
+                    }
+
+print(university_dictFive)
