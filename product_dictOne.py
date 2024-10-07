70a4b2a597aedd2d01bda487e821071585d24f1d
shamcalcetas <calcetasshamelle@gmail.com>
2024-10-07 23:10:29 +0800

    docs: adding product_dictOne.py

---
+++ product_dictOne.py
@@ -0,0 +1,54 @@
+product_dictOne = {
+                    "ID": "1",
+                    "Name": "Laptop",
+                    "Category": "Electronics",
+                    "Price": "750",
+                    "Stock": "50",
+                    "Supplier Email": "supplier1@gmail.com"
+                  }
+                  
+print(product_dictOne)
+
+product_dictTwo = {
+                    "ID": "2",
+                    "Name": "Desk Chair",
+                    "Category": "Furniture",
+                    "Price": "100",
+                    "Stock": "200",
+                    "Supplier Email": "supplier2@gmail.com"
+                  }
+
+print(product_dictTwo)
+
+product_dictThree = {
+                      "ID": "3",
+                      "Name": "Smartwatch",
+                      "Category": "Electronics",
+                      "Price": "200",
+                      "Stock": "150",
+                      "Supplier Email": "supplier3@gmail.com"
+                  }
+
+print(product_dictThree)
+
+product_dictFour = {
+                      "ID": "4",
+                      "Name": "Notebook",
+                      "Category": "Stationery",
+                      "Price": "5",
+                      "Stock": "500",
+                      "Supplier Email": "supplier4@gmail.com"
+                    }
+
+print(product_dictFour)
+
+product_dictFive = {
+                      "ID": "5",
+                      "Name": "Running Shoes",
+                      "Category": "Apparel",
+                      "Price": "80",
+                      "Stock": "100",
+                      "Supplier Email": "supplier5@gmail.com"
+                  }
+
+print(product_dictFive)
