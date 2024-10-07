8668dbed503dc580c71f95d4abff42ee564fab22
shamcalcetas <calcetasshamelle@gmail.com>
2024-10-07 23:18:08 +0800

    docs: adding employee_dict.py

---
+++ employee_dict.py
@@ -0,0 +1,49 @@
+employee_dictOne = {
+                    "ID" : "1",
+                    "Name" : "John Doe",
+                    "Department" : "Sales",
+                    "Age" : "30",
+                    "Email" : "john.doe@company.com"
+                }
+
+print(employee_dictOne)
+
+employee_dictTwo = {
+                    "ID" : "2",
+                    "Name" : "Jane Smith",
+                    "Department" : "Human Resources",
+                    "Age" : "25",
+                    "Email" : "jane.smith@company.com"
+                }
+
+print(employee_dictTwo)
+
+employee_dictThree = {
+                    "ID" : "3",
+                    "Name" : "Mark Johnson",
+                    "Department" : "IT",
+                    "Age" : "40",
+                    "Email" : "mark.johnson@company.com"
+                }
+
+print(employee_dictThree)
+
+employee_dictFour = {
+                    "ID" : "4",
+                    "Name" : "Lisa Wong",
+                    "Department" : "Marketing",
+                    "Age" : "28",
+                    "Email" : "lisa.wong@company.com"
+                }
+
+print(employee_dictFour)
+
+employee_dictFive = {
+                    "ID" : "5",
+                    "Name" : "Paul McDonald",
+                    "Department" : "Finance",
+                    "Age" : "28",
+                    "Email" : "paul.mcdonald@company.com"
+                }
+
+print(employee_dictFive)
