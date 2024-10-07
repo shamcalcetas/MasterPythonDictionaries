7ad10f2da230a055558a876b7631f905140c4e6f
shamcalcetas <calcetasshamelle@gmail.com>
2024-10-07 23:21:58 +0800

    docs: adding restaurant_dict.py

---
+++ restaurant_dict.py
@@ -0,0 +1,55 @@
+restaurant_dictOne = {
+                        "ID" : "1",
+                        "Name" : "Viking Luxury Buffet",
+                        "Location" : "Pasay City",
+                        "Cuisine Type" : "Buffet",
+                        "Established Year" : "2011",
+                        "Webiste or Contact" : "www.vikings.ph"
+                    }
+
+print(restaurant_dictOne)
+
+restaurant_dictTwo = {
+                        "ID" : "2",
+                        "Name" : "Antonio's Restaurant",
+                        "Location" : "Tagaytay",
+                        "Cuisine Type" : "Fine Dining",
+                        "Established Year" : "2002",
+                        "Webiste or Contact" : "www.antoniosrestaurant.ph"
+                    }
+
+print(restaurant_dictTwo)
+
+restaurant_dictThree = {
+                          "ID" : "3",
+                          "Name" : "Mesa Filipino Moderne",
+                          "Location" : "Makati City",
+                          "Cuisine Type" : "Filipino",
+                          "Established Year" : "2009",
+                          "Webiste or Contact" : "www.mesa.ph"
+                      }
+
+print(restaurant_dictThree)
+
+restaurant_dictFour = {
+                        "ID" : "4",
+                        "Name" : "Manam Comfort Filipino",
+                        "Location" : "Quezon City",
+                        "Cuisine Type" : "Filipino",
+                        "Established Year" : "2013",
+                        "Webiste or Contact" : "www.manam.ph"
+                    }
+
+print(restaurant_dictFour)
+
+
+restaurant_dictFive = {
+                        "ID" : "5",
+                        "Name" : "Ramen Nagi",
+                        "Location" : "Various locations",
+                        "Cuisine Type" : "Japanese",
+                        "Established Year" : "2013",
+                        "Webiste or Contact" : "www.ramennagi.com.ph"
+                      }
+
+print(restaurant_dictFive)
